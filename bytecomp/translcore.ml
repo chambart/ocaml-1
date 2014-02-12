d5e327ea0511d643aafab31d7a7f3011cf17daa7
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-02-12 22:58:25 +0100

    Fix lazy and exceptions allocated as immutable blocks
    This didn't matter until now, but some passes could assume that
    immutable block are not mutated. For exception that means that
    structuraly equal exceptions can't be shared, hence preventing
    exceptions declared by a functor to be staticaly allocated.

--- bytecomp/translcore.ml
+++ bytecomp/translcore.ml
@@ -871,7 +871,7 @@ and transl_exp0 e =
       (* other cases compile to a lazy block holding a function *)
       | _ ->
           let fn = Lfunction (Curried, [Ident.create "param"], transl_exp e) in
-          Lprim(Pmakeblock(Config.lazy_tag, Immutable), [fn])
+          Lprim(Pmakeblock(Config.lazy_tag, Mutable), [fn])
       end
   | Texp_object (cs, meths) ->
       let cty = cs.cstr_type in
@@ -1088,7 +1088,7 @@ let transl_exception id path decl =
     match path with
       None -> Ident.name id
     | Some p -> Path.name p in
-  Lprim(Pmakeblock(0, Immutable), [Lconst(Const_base(Const_string name))])
+  Lprim(Pmakeblock(0, Mutable), [Lconst(Const_base(Const_string name))])
 
 (* Error report *)
 
