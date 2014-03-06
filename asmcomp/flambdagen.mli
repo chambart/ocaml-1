1a06b6c08923ecb97f9a4e758119b2670033db92
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-03-06 01:50:21 +0100

    Fix 3: copyright year 2013 -> 2014

--- asmcomp/flambdagen.mli
+++ asmcomp/flambdagen.mli
@@ -4,7 +4,7 @@
 (*                                                                        *)
 (*                      Pierre Chambart (OCamlPro)                        *)
 (*                                                                        *)
-(*   Copyright 2013 Institut National de Recherche en Informatique et     *)
+(*   Copyright 2014 Institut National de Recherche en Informatique et     *)
 (*   en Automatique.  All rights reserved.  This file is distributed      *)
 (*   under the terms of the Q Public License version 1.0.                 *)
 (*                                                                        *)
