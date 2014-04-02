chambart/ocaml

1 - introduce flambda type

asmcomp/printflambda.ml
@@ -1,0 +1,172 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                     Pierre Chambart, OCamlPro                       *)
+(*                                                                     *)
+(*  Copyright 2014 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+
+open Format
+open Ext_types
+open Flambda
+
+let rec lam ppf = function
+  | Fsymbol (symbol,_) ->
+      Symbol.print ppf symbol
+  | Fvar (id,_) ->
+      Variable.print ppf id
+  | Fconst (cst,_) ->
+      const ppf cst
+  | Fapply({ap_function; ap_arg; ap_kind},_) ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      let direct = match ap_kind with Indirect -> "" | Direct _ -> "*" in
+      fprintf ppf "@[<2>(apply%s@ %a%a)@]" direct lam ap_function lams ap_arg
+  | Ffunction({fu_closure;fu_fun;fu_relative_to = None},_) ->
+      fprintf ppf "@[<2>(function@ %a@ %a)@]" Closure_function.print fu_fun lam fu_closure
+  | Ffunction({fu_closure;fu_fun;fu_relative_to = Some rel},_) ->
+      fprintf ppf "@[<2>(function_relative@ %a - %a@ %a)@]"
+        Closure_function.print fu_fun Closure_function.print rel lam fu_closure
+  | Fvariable_in_closure({vc_closure;vc_fun;vc_var},_) ->
+      fprintf ppf "@[<2>(var@ %a@ %a@ %a)@]"
+        Closure_variable.print vc_var Closure_function.print vc_fun lam vc_closure
+  | Fclosure({cl_fun;cl_free_var;cl_specialised_arg},_) ->
+      let idents ppf =
+        List.iter (fprintf ppf "@ %a" Variable.print) in
+      let one_fun ppf f =
+        fprintf ppf "(closure@ %a@ %d@ @[<2>%a@]@ @[<2>%a@])"
+          Function_label.print f.label f.arity idents f.params lam f.body in
+      let funs ppf =
+        VarMap.iter (fun _ v -> fprintf ppf "@ %a" one_fun v) in
+      let lams ppf =
+        VarMap.iter (fun id v -> fprintf ppf "@ %a = %a"
+                        Variable.print id lam v) in
+      let spec ppf spec_args =
+        if not (VarMap.is_empty spec_args)
+        then begin
+          fprintf ppf "@ with";
+          VarMap.iter (fun id id' -> fprintf ppf "@ %a <- %a"
+                          Variable.print id Variable.print id')
+            spec_args
+        end
+      in
+      fprintf ppf "@[<2>(closure%a %a%a)@]" funs cl_fun.funs lams
+        cl_free_var spec cl_specialised_arg
+  | Flet(str, id, arg, body,_) ->
+      let rec letbody ul = match ul with
+        | Flet(str, id, arg, body,_) ->
+            fprintf ppf "@ @[<2>%a@ %a@]" Variable.print id lam arg;
+            letbody body
+        | _ -> ul in
+      fprintf ppf "@[<2>(let@ @[<hv 1>(@[<2>%a@ %a@]" Variable.print id lam arg;
+      let expr = letbody body in
+      fprintf ppf ")@]@ %a)@]" lam expr
+  | Fletrec(id_arg_list, body,_) ->
+      let bindings ppf id_arg_list =
+        let spc = ref false in
+        List.iter
+          (fun (id, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<2>%a@ %a@]" Variable.print id lam l)
+          id_arg_list in
+      fprintf ppf
+        "@[<2>(letrec@ (@[<hv 1>%a@])@ %a)@]" bindings id_arg_list lam body
+  | Fprim(prim, largs, _,_) ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      fprintf ppf "@[<2>(%a%a)@]" Printlambda.primitive prim lams largs
+  | Fswitch(larg, sw,_) ->
+      let switch ppf sw =
+        let spc = ref false in
+        List.iter
+          (fun (n, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<hv 1>case int %i:@ %a@]" n lam l)
+          sw.fs_consts;
+        List.iter
+          (fun (n, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<hv 1>case tag %i:@ %a@]" n lam l)
+          sw.fs_blocks ;
+        begin match sw.fs_failaction with
+        | None  -> ()
+        | Some l ->
+            if !spc then fprintf ppf "@ " else spc := true;
+            fprintf ppf "@[<hv 1>default:@ %a@]" lam l
+        end in
+      fprintf ppf
+        "@[<1>(%s(%i,%i) %a@ @[<v 0>%a@])@]"
+        (match sw.fs_failaction with None -> "switch*" | _ -> "switch")
+        (IntSet.cardinal sw.fs_numconsts)
+        (IntSet.cardinal sw.fs_numblocks)
+        lam larg switch sw
+  | Fstaticfail (i, ls,_)  ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      fprintf ppf "@[<2>(exit@ %d%a)@]" i lams ls;
+  | Fcatch(i, vars, lbody, lhandler,_) ->
+      fprintf ppf "@[<2>(catch@ %a@;<1 -1>with (%d%a)@ %a)@]"
+        lam lbody i
+        (fun ppf vars -> match vars with
+           | [] -> ()
+           | _ ->
+               List.iter
+                 (fun x -> fprintf ppf " %a" Variable.print x)
+                 vars)
+        vars
+        lam lhandler
+  | Ftrywith(lbody, param, lhandler,_) ->
+      fprintf ppf "@[<2>(try@ %a@;<1 -1>with %a@ %a)@]"
+        lam lbody Variable.print param lam lhandler
+  | Fifthenelse(lcond, lif, lelse,_) ->
+      fprintf ppf "@[<2>(if@ %a@ %a@ %a)@]" lam lcond lam lif lam lelse
+  | Fsequence(l1, l2,_) ->
+      fprintf ppf "@[<2>(seq@ %a@ %a)@]" lam l1 sequence l2
+  | Fwhile(lcond, lbody,_) ->
+      fprintf ppf "@[<2>(while@ %a@ %a)@]" lam lcond lam lbody
+  | Ffor(param, lo, hi, dir, body,_) ->
+      fprintf ppf "@[<2>(for %a@ %a@ %s@ %a@ %a)@]"
+        Variable.print param lam lo
+        (match dir with Asttypes.Upto -> "to" | Asttypes.Downto -> "downto")
+        lam hi lam body
+  | Fassign(id, expr,_) ->
+      fprintf ppf "@[<2>(assign@ %a@ %a)@]" Variable.print id lam expr
+  | Fsend (k, met, obj, largs, _,_) ->
+      let args ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      let kind =
+        if k = Lambda.Self then "self" else if k = Lambda.Cached then "cache" else "" in
+      fprintf ppf "@[<2>(send%s@ %a@ %a%a)@]" kind lam obj lam met args largs
+  | Funreachable _ ->
+      fprintf ppf "unreachable"
+
+and sequence ppf ulam = match ulam with
+  | Fsequence(l1, l2,_) ->
+      fprintf ppf "%a@ %a" sequence l1 sequence l2
+  | _ -> lam ppf ulam
+
+and const ppf c = let open Asttypes in match c with
+  | Fconst_base(Const_int n) -> fprintf ppf "%i" n
+  | Fconst_base(Const_char c) -> fprintf ppf "%C" c
+  | Fconst_base(Const_string s) -> fprintf ppf "%S" s
+  | Fconst_immstring s -> fprintf ppf "#%S" s
+  | Fconst_base(Const_float f) -> fprintf ppf "%s" f
+  | Fconst_base(Const_int32 n) -> fprintf ppf "%lil" n
+  | Fconst_base(Const_int64 n) -> fprintf ppf "%LiL" n
+  | Fconst_base(Const_nativeint n) -> fprintf ppf "%nin" n
+  | Fconst_pointer n -> fprintf ppf "%ia" n
+  | Fconst_float_array [] ->
+      fprintf ppf "[| |]"
+  | Fconst_float_array (f1 :: fl) ->
+      let floats ppf fl =
+        List.iter (fun f -> fprintf ppf "@ %s" f) fl in
+      fprintf ppf "@[<1>[|@[%s%a@]|]@]" f1 floats fl
+
+let flambda ppf ulam =
+  fprintf ppf "%a@." lam ulam

utils/ext_types.ml
@@ -1,0 +1,229 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*                     Pierre Chambart, OCamlPro                       *)
+(*                                                                     *)
+(*  Copyright 2014 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+open Misc
+
+module type PrintableHashOrdered = sig
+  type t
+  val compare : t -> t -> int
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+  val hash : t -> int
+  val equal : t -> t -> bool
+end
+
+module type ExtMap = sig
+  module M : PrintableHashOrdered
+  include Map.S with type key = M.t
+                 and type 'a t = 'a Map.Make(M).t
+  val map_option : (key -> 'a -> 'b option) -> 'a t -> 'b t
+  val of_list : (key * 'a) list -> 'a t
+  val disjoint_union : ?eq:('a -> 'a -> bool) -> 'a t -> 'a t -> 'a t
+  val last_union : 'a t -> 'a t -> 'a t
+  val rename : key t -> key -> key
+  val map_keys : (key -> key) -> 'a t -> 'a t
+  val print :
+    (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+end
+
+module type ExtSet = sig
+  module M : PrintableHashOrdered
+  include Set.S with type elt = M.t
+                 and type t = Set.Make(M).t
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+  val to_string : t -> string
+  val of_list : elt list -> t
+  val map : (elt -> elt) -> t -> t
+end
+
+module type ExtHashtbl = sig
+  module M : PrintableHashOrdered
+  include Hashtbl.S with type key = M.t
+                     and type 'a t = 'a Hashtbl.Make(M).t
+  val to_map : 'a t -> 'a Map.Make(M).t
+  val memoize : 'a t -> (key -> 'a) -> key -> 'a
+end
+
+
+module ExtMap(M:PrintableHashOrdered) : ExtMap with module M := M =
+struct
+  include Map.Make(M)
+  let map_option f m =
+    fold (fun id v map ->
+        match f id v with
+        | None -> map
+        | Some r -> add id r map) m empty
+  let of_list l =
+    List.fold_left (fun map (id,v) -> add id v map) empty l
+  let disjoint_union ?eq m1 m2 =
+    merge (fun id x y -> match x, y with
+        | None, None -> None
+        | None, Some v | Some v, None -> Some v
+        | Some v1, Some v2 ->
+            let ok = match eq with
+              | None -> false
+              | Some eq -> eq v1 v2 in
+            if not ok
+            then
+              let err = Format.asprintf "ExtMap.disjoint_union %a" M.print id in
+              fatal_error err
+            else Some v1) m1 m2
+
+  let last_union m1 m2 =
+    merge (fun id x y -> match x, y with
+        | None, None -> None
+        | None, Some v
+        | Some v, None
+        | Some _, Some v -> Some v) m1 m2
+
+  let rename m v =
+    try find v m with Not_found -> v
+  let map_keys f m =
+    of_list (List.map (fun (k,v) -> f k, v) (bindings m))
+  let print f ppf s =
+    let elts ppf s = iter (fun id v ->
+        Format.fprintf ppf "@ (%a %a)" M.print id f v) s in
+    Format.fprintf ppf "@[<1>{@[%a@ @]}@]" elts s
+end
+
+module ExtSet(M:PrintableHashOrdered) : ExtSet with module M := M =
+struct
+  include Set.Make(M)
+  let output oc s =
+    Printf.fprintf oc "( ";
+    iter (fun v -> Printf.fprintf oc "%a " M.output v) s;
+    Printf.fprintf oc ")"
+  let print ppf s =
+    let elts ppf s = iter (fun e -> Format.fprintf ppf "@ %a" M.print e) s in
+    Format.fprintf ppf "@[<1>{@[%a@ @]}@]" elts s
+  let to_string s = Format.asprintf "%a" print s
+  let of_list l = match l with
+    | [] -> empty
+    | [t] -> singleton t
+    | t :: q -> List.fold_left (fun acc e -> add e acc) (singleton t) q
+  let map f s = of_list (List.map f (elements s))
+end
+
+module ExtHashtbl(M:PrintableHashOrdered) : ExtHashtbl with module M := M =
+struct
+  include Hashtbl.Make(M)
+  module MMap = Map.Make(M)
+  let to_map v = fold MMap.add v MMap.empty
+  let memoize t f = fun key ->
+    try find t key with
+    | Not_found ->
+        let r = f key in
+        add t key r;
+        r
+end
+
+module type Empty = sig end
+
+module type BaseId = sig
+  type t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val hash : t -> int
+  val name : t -> string option
+  val to_string : t -> string
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+end
+
+module type Id = sig
+  include BaseId
+  val create : ?name:string -> unit -> t
+end
+
+module type UnitId = sig
+  include BaseId
+  val create : ?name:string -> string -> t
+  val unit : t -> string
+end
+
+module Id(E:Empty) : Id = struct
+  type t = int * string
+  let empty_string = ""
+  let create = let r = ref 0 in
+    fun  ?(name=empty_string) () -> incr r; !r, name
+  let equal (t1,_) (t2,_) = (t1:int) = t2
+  let compare (t1,_) (t2,_) = t1 - t2
+  let hash (t,_) = t
+  let name (_,name) =
+    if name == empty_string
+    then None
+    else Some name
+  let to_string (t,name) =
+    if name == empty_string
+    then string_of_int t
+    else Printf.sprintf "%s_%i" name t
+  let output fd t = output_string fd (to_string t)
+  let print ppf v = Format.pp_print_string ppf (to_string v)
+end
+
+module UnitId(Innerid:Id) : UnitId = struct
+  type t = {
+    id : Innerid.t;
+    unit : string;
+  }
+  let compare x y =
+    let c = Innerid.compare x.id y.id in
+    if c <> 0
+    then c
+    else String.compare x.unit y.unit
+  let output oc x =
+    Printf.fprintf oc "%s.%a"
+      x.unit
+      Innerid.output x.id
+  let print ppf x =
+    Format.fprintf ppf "%s.%a"
+      x.unit
+      Innerid.print x.id
+  let hash off = Hashtbl.hash off
+  let equal o1 o2 = compare o1 o2 = 0
+  let name o = Innerid.name o.id
+  let to_string x =
+    Printf.sprintf "%s.%s"
+      x.unit
+      (Innerid.to_string x.id)
+  let create ?name unit =
+    let id = Innerid.create ?name () in
+    { id; unit }
+  let unit x = x.unit
+end
+
+module Int = struct
+  type t = int
+  let compare x y = x - y
+  let output oc x = Printf.fprintf oc "%i" x
+  let hash i = i
+  let equal (i:int) j = i = j
+  let print = Format.pp_print_int
+end
+
+module String_M = struct
+  type t = string
+  let compare = String.compare
+  let output = output_string
+  let hash (s:string) = Hashtbl.hash s
+  let equal (s1:string) s2 = s1 = s2
+  let print = Format.pp_print_string
+end
+
+module IntSet = ExtSet(Int)
+module IntMap = ExtMap(Int)
+module IntTbl = ExtHashtbl(Int)
+
+module StringSet = ExtSet(String_M)
+module StringMap = ExtMap(String_M)
+module StringTbl = ExtHashtbl(String_M)

chambart/ocaml-1

asmcomp/printflambda.ml
@@ -1,0 +1,171 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                     Pierre Chambart, OCamlPro                       *)
+(*                                                                     *)
+(*  Copyright 2014 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+
+open Format
+open Ext_types
+open Flambda
+
+let rec lam ppf = function
+  | Fsymbol (symbol,_) ->
+      Symbol.print ppf symbol
+  | Fvar (id,_) ->
+      Variable.print ppf id
+  | Fconst (cst,_) ->
+      const ppf cst
+  | Fapply({ap_function; ap_arg; ap_kind},_) ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      let direct = match ap_kind with Indirect -> "" | Direct _ -> "*" in
+      fprintf ppf "@[<2>(apply%s@ %a%a)@]" direct lam ap_function lams ap_arg
+  | Ffunction({fu_closure;fu_fun;fu_relative_to = None},_) ->
+      fprintf ppf "@[<2>(function@ %a@ %a)@]" Closure_function.print fu_fun lam fu_closure
+  | Ffunction({fu_closure;fu_fun;fu_relative_to = Some rel},_) ->
+      fprintf ppf "@[<2>(function_relative@ %a - %a@ %a)@]"
+        Closure_function.print fu_fun Closure_function.print rel lam fu_closure
+  | Fvariable_in_closure({vc_closure;vc_fun;vc_var},_) ->
+      fprintf ppf "@[<2>(var@ %a@ %a@ %a)@]"
+        Closure_variable.print vc_var Closure_function.print vc_fun lam vc_closure
+  | Fclosure({cl_fun;cl_free_var;cl_specialised_arg},_) ->
+      let idents ppf =
+        List.iter (fprintf ppf "@ %a" Variable.print) in
+      let funs ppf =
+        VarMap.iter (fun var f ->
+            fprintf ppf "@ (fun@ %a@[<2>%a@]@ @[<2>%a@])"
+              Variable.print var idents f.params lam f.body) in
+      let lams ppf =
+        VarMap.iter (fun id v -> fprintf ppf "@ %a = %a"
+                        Variable.print id lam v) in
+      let spec ppf spec_args =
+        if not (VarMap.is_empty spec_args)
+        then begin
+          fprintf ppf "@ with";
+          VarMap.iter (fun id id' -> fprintf ppf "@ %a <- %a"
+                          Variable.print id Variable.print id')
+            spec_args
+        end
+      in
+      fprintf ppf "@[<2>(closure%a %a%a)@]" funs cl_fun.funs lams
+        cl_free_var spec cl_specialised_arg
+  | Flet(str, id, arg, body,_) ->
+      let rec letbody ul = match ul with
+        | Flet(str, id, arg, body,_) ->
+            fprintf ppf "@ @[<2>%a@ %a@]" Variable.print id lam arg;
+            letbody body
+        | _ -> ul in
+      fprintf ppf "@[<2>(let@ @[<hv 1>(@[<2>%a@ %a@]" Variable.print id lam arg;
+      let expr = letbody body in
+      fprintf ppf ")@]@ %a)@]" lam expr
+  | Fletrec(id_arg_list, body,_) ->
+      let bindings ppf id_arg_list =
+        let spc = ref false in
+        List.iter
+          (fun (id, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<2>%a@ %a@]" Variable.print id lam l)
+          id_arg_list in
+      fprintf ppf
+        "@[<2>(letrec@ (@[<hv 1>%a@])@ %a)@]" bindings id_arg_list lam body
+  | Fprim(prim, largs, _,_) ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      fprintf ppf "@[<2>(%a%a)@]" Printlambda.primitive prim lams largs
+  | Fswitch(larg, sw,_) ->
+      let switch ppf sw =
+        let spc = ref false in
+        List.iter
+          (fun (n, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<hv 1>case int %i:@ %a@]" n lam l)
+          sw.fs_consts;
+        List.iter
+          (fun (n, l) ->
+             if !spc then fprintf ppf "@ " else spc := true;
+             fprintf ppf "@[<hv 1>case tag %i:@ %a@]" n lam l)
+          sw.fs_blocks ;
+        begin match sw.fs_failaction with
+        | None  -> ()
+        | Some l ->
+            if !spc then fprintf ppf "@ " else spc := true;
+            fprintf ppf "@[<hv 1>default:@ %a@]" lam l
+        end in
+      fprintf ppf
+        "@[<1>(%s(%i,%i) %a@ @[<v 0>%a@])@]"
+        (match sw.fs_failaction with None -> "switch*" | _ -> "switch")
+        (IntSet.cardinal sw.fs_numconsts)
+        (IntSet.cardinal sw.fs_numblocks)
+        lam larg switch sw
+  | Fstaticfail (i, ls,_)  ->
+      let lams ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      fprintf ppf "@[<2>(exit@ %a%a)@]" Static_exception.print i lams ls;
+  | Fcatch(i, vars, lbody, lhandler,_) ->
+      fprintf ppf "@[<2>(catch@ %a@;<1 -1>with (%a%a)@ %a)@]"
+        lam lbody Static_exception.print i
+        (fun ppf vars -> match vars with
+           | [] -> ()
+           | _ ->
+               List.iter
+                 (fun x -> fprintf ppf " %a" Variable.print x)
+                 vars)
+        vars
+        lam lhandler
+  | Ftrywith(lbody, param, lhandler,_) ->
+      fprintf ppf "@[<2>(try@ %a@;<1 -1>with %a@ %a)@]"
+        lam lbody Variable.print param lam lhandler
+  | Fifthenelse(lcond, lif, lelse,_) ->
+      fprintf ppf "@[<2>(if@ %a@ %a@ %a)@]" lam lcond lam lif lam lelse
+  | Fsequence(l1, l2,_) ->
+      fprintf ppf "@[<2>(seq@ %a@ %a)@]" lam l1 sequence l2
+  | Fwhile(lcond, lbody,_) ->
+      fprintf ppf "@[<2>(while@ %a@ %a)@]" lam lcond lam lbody
+  | Ffor(param, lo, hi, dir, body,_) ->
+      fprintf ppf "@[<2>(for %a@ %a@ %s@ %a@ %a)@]"
+        Variable.print param lam lo
+        (match dir with Asttypes.Upto -> "to" | Asttypes.Downto -> "downto")
+        lam hi lam body
+  | Fassign(id, expr,_) ->
+      fprintf ppf "@[<2>(assign@ %a@ %a)@]" Variable.print id lam expr
+  | Fsend (k, met, obj, largs, _,_) ->
+      let args ppf largs =
+        List.iter (fun l -> fprintf ppf "@ %a" lam l) largs in
+      let kind =
+        if k = Lambda.Self then "self" else if k = Lambda.Cached then "cache" else "" in
+      fprintf ppf "@[<2>(send%s@ %a@ %a%a)@]" kind lam obj lam met args largs
+  | Funreachable _ ->
+      fprintf ppf "unreachable"
+
+and sequence ppf ulam = match ulam with
+  | Fsequence(l1, l2,_) ->
+      fprintf ppf "%a@ %a" sequence l1 sequence l2
+  | _ -> lam ppf ulam
+
+and const ppf c = let open Asttypes in match c with
+  | Fconst_base(Const_int n) -> fprintf ppf "%i" n
+  | Fconst_base(Const_char c) -> fprintf ppf "%C" c
+  | Fconst_base(Const_string s) -> fprintf ppf "%S" s
+  | Fconst_immstring s -> fprintf ppf "#%S" s
+  | Fconst_base(Const_float f) -> fprintf ppf "%s" f
+  | Fconst_base(Const_int32 n) -> fprintf ppf "%lil" n
+  | Fconst_base(Const_int64 n) -> fprintf ppf "%LiL" n
+  | Fconst_base(Const_nativeint n) -> fprintf ppf "%nin" n
+  | Fconst_pointer n -> fprintf ppf "%ia" n
+  | Fconst_float_array [] ->
+      fprintf ppf "[| |]"
+  | Fconst_float_array (f1 :: fl) ->
+      let floats ppf fl =
+        List.iter (fun f -> fprintf ppf "@ %s" f) fl in
+      fprintf ppf "@[<1>[|@[%s%a@]|]@]" f1 floats fl
+
+let flambda ppf ulam =
+  fprintf ppf "%a@." lam ulam

utils/ext_types.ml
@@ -1,0 +1,238 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*                     Pierre Chambart, OCamlPro                       *)
+(*                                                                     *)
+(*  Copyright 2014 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+open Misc
+
+module type PrintableHashOrdered = sig
+  type t
+  val compare : t -> t -> int
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+  val hash : t -> int
+  val equal : t -> t -> bool
+end
+
+module type ExtMap = sig
+  module M : PrintableHashOrdered
+  include Map.S with type key = M.t
+                 and type 'a t = 'a Map.Make(M).t
+  val map_option : (key -> 'a -> 'b option) -> 'a t -> 'b t
+  val of_list : (key * 'a) list -> 'a t
+  val disjoint_union : ?eq:('a -> 'a -> bool) -> 'a t -> 'a t -> 'a t
+  val union_right : 'a t -> 'a t -> 'a t
+  val union_left : 'a t -> 'a t -> 'a t
+  val rename : key t -> key -> key
+  val map_keys : (key -> key) -> 'a t -> 'a t
+  val keys : 'a t -> Set.Make(M).t
+  val print :
+    (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+end
+
+module type ExtSet = sig
+  module M : PrintableHashOrdered
+  include Set.S with type elt = M.t
+                 and type t = Set.Make(M).t
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+  val to_string : t -> string
+  val of_list : elt list -> t
+  val map : (elt -> elt) -> t -> t
+end
+
+module type ExtHashtbl = sig
+  module M : PrintableHashOrdered
+  include Hashtbl.S with type key = M.t
+                     and type 'a t = 'a Hashtbl.Make(M).t
+  val to_map : 'a t -> 'a Map.Make(M).t
+  val memoize : 'a t -> (key -> 'a) -> key -> 'a
+end
+
+
+module ExtMap(M:PrintableHashOrdered) : ExtMap with module M := M =
+struct
+  include Map.Make(M)
+  let map_option f m =
+    fold (fun id v map ->
+        match f id v with
+        | None -> map
+        | Some r -> add id r map) m empty
+  let of_list l =
+    List.fold_left (fun map (id,v) -> add id v map) empty l
+  let disjoint_union ?eq m1 m2 =
+    merge (fun id x y -> match x, y with
+        | None, None -> None
+        | None, Some v | Some v, None -> Some v
+        | Some v1, Some v2 ->
+            let ok = match eq with
+              | None -> false
+              | Some eq -> eq v1 v2 in
+            if not ok
+            then
+              let err = Format.asprintf "ExtMap.disjoint_union %a" M.print id in
+              fatal_error err
+            else Some v1) m1 m2
+
+  let union_right m1 m2 =
+    merge (fun id x y -> match x, y with
+        | None, None -> None
+        | None, Some v
+        | Some v, None
+        | Some _, Some v -> Some v) m1 m2
+
+  let union_left m1 m2 = union_right m2 m1
+
+  let rename m v =
+    try find v m with Not_found -> v
+  let map_keys f m =
+    of_list (List.map (fun (k,v) -> f k, v) (bindings m))
+  let print f ppf s =
+    let elts ppf s = iter (fun id v ->
+        Format.fprintf ppf "@ (%a %a)" M.print id f v) s in
+    Format.fprintf ppf "@[<1>{@[%a@ @]}@]" elts s
+
+  module MSet = Set.Make(M)
+
+  let keys map = fold (fun k _ set -> MSet.add k set) map MSet.empty
+
+end
+
+module ExtSet(M:PrintableHashOrdered) : ExtSet with module M := M =
+struct
+  include Set.Make(M)
+  let output oc s =
+    Printf.fprintf oc "( ";
+    iter (fun v -> Printf.fprintf oc "%a " M.output v) s;
+    Printf.fprintf oc ")"
+  let print ppf s =
+    let elts ppf s = iter (fun e -> Format.fprintf ppf "@ %a" M.print e) s in
+    Format.fprintf ppf "@[<1>{@[%a@ @]}@]" elts s
+  let to_string s = Format.asprintf "%a" print s
+  let of_list l = match l with
+    | [] -> empty
+    | [t] -> singleton t
+    | t :: q -> List.fold_left (fun acc e -> add e acc) (singleton t) q
+  let map f s = of_list (List.map f (elements s))
+end
+
+module ExtHashtbl(M:PrintableHashOrdered) : ExtHashtbl with module M := M =
+struct
+  include Hashtbl.Make(M)
+  module MMap = Map.Make(M)
+  let to_map v = fold MMap.add v MMap.empty
+  let memoize t f = fun key ->
+    try find t key with
+    | Not_found ->
+        let r = f key in
+        add t key r;
+        r
+end
+
+module type BaseId = sig
+  type t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val hash : t -> int
+  val name : t -> string option
+  val to_string : t -> string
+  val output : out_channel -> t -> unit
+  val print : Format.formatter -> t -> unit
+end
+
+module type Id = sig
+  include BaseId
+  val create : ?name:string -> unit -> t
+end
+
+module type UnitId = sig
+  module Compilation_unit : PrintableHashOrdered
+  include BaseId
+  val create : ?name:string -> Compilation_unit.t -> t
+  val unit : t -> Compilation_unit.t
+end
+
+module Id(E:sig end) : Id = struct
+  type t = int * string
+  let empty_string = ""
+  let create = let r = ref 0 in
+    fun  ?(name=empty_string) () -> incr r; !r, name
+  let equal (t1,_) (t2,_) = (t1:int) = t2
+  let compare (t1,_) (t2,_) = t1 - t2
+  let hash (t,_) = t
+  let name (_,name) =
+    if name == empty_string
+    then None
+    else Some name
+  let to_string (t,name) =
+    if name == empty_string
+    then string_of_int t
+    else Printf.sprintf "%s_%i" name t
+  let output fd t = output_string fd (to_string t)
+  let print ppf v = Format.pp_print_string ppf (to_string v)
+end
+
+module UnitId(Innerid:Id)(Compilation_unit:PrintableHashOrdered) :
+  UnitId with module Compilation_unit := Compilation_unit = struct
+  type t = {
+    id : Innerid.t;
+    unit : Compilation_unit.t;
+  }
+  let compare x y =
+    let c = Innerid.compare x.id y.id in
+    if c <> 0
+    then c
+    else Compilation_unit.compare x.unit y.unit
+  let output oc x =
+    Printf.fprintf oc "%a.%a"
+      Compilation_unit.output x.unit
+      Innerid.output x.id
+  let print ppf x =
+    Format.fprintf ppf "%a.%a"
+      Compilation_unit.print x.unit
+      Innerid.print x.id
+  let hash off = Hashtbl.hash off
+  let equal o1 o2 = compare o1 o2 = 0
+  let name o = Innerid.name o.id
+  let to_string x =
+    Format.asprintf "%a.%a"
+      Compilation_unit.print x.unit
+      Innerid.print x.id
+  let create ?name unit =
+    let id = Innerid.create ?name () in
+    { id; unit }
+  let unit x = x.unit
+end
+
+module Int = struct
+  type t = int
+  let compare x y = x - y
+  let output oc x = Printf.fprintf oc "%i" x
+  let hash i = i
+  let equal (i:int) j = i = j
+  let print = Format.pp_print_int
+end
+
+module String_M = struct
+  type t = string
+  let compare = String.compare
+  let output = output_string
+  let hash (s:string) = Hashtbl.hash s
+  let equal (s1:string) s2 = s1 = s2
+  let print = Format.pp_print_string
+end
+
+module IntSet = ExtSet(Int)
+module IntMap = ExtMap(Int)
+module IntTbl = ExtHashtbl(Int)
+
+module StringSet = ExtSet(String_M)
+module StringMap = ExtMap(String_M)
+module StringTbl = ExtHashtbl(String_M)