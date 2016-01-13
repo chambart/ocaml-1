49804d064f70f3dff14b30c8a73a56e49391d03c
Mark Shinwell <mshinwell@gmail.com>
2016-01-13 10:44:15 +0100

    remove duplicate declaration

--- parsing/location.ml
+++ parsing/location.ml
@@ -431,8 +431,6 @@ let () =
 
 external reraise : exn -> 'a = "%reraise"
 
-external reraise : exn -> 'a = "%reraise"
-
 let rec report_exception_rec n ppf exn =
   try match error_of_exn exn with
   | Some err ->
