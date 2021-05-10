903e8d4a641a4fca05614b660a2f2c9cd19e03d1
Guillaume Bury <guillaume.bury@gmail.com>
2021-05-10 13:59:25 +0200

    Update test to use names instead of line numbers
    LUsing lne numbers in the output of a test is quite brittle,
    as simply adding a comment can beeak the test, so we here use
    (unique) test names instead of line numbers.

--- testsuite/tests/flambda-unboxing/rec_unboxing.ml
+++ testsuite/tests/flambda-unboxing/rec_unboxing.ml
@@ -20,30 +20,30 @@
 external minor_words : unit -> (float [@unboxed])
   = "caml_gc_minor_words" "caml_gc_minor_words_unboxed"
 
-let[@inline never] check_no_alloc line f x y expected_res =
+let[@inline never] check_no_alloc test_name f x y expected_res =
   let before = minor_words () in
   let b = (f[@inlined never]) x y expected_res in
   let after = minor_words () in
   let diff = after -. before in
   if not b then
-    Format.printf "Bad assert at line %d@." line
+    Format.printf "Bad assert for test '%s'@." test_name
   else if diff = 0. then
-    Format.printf "No allocs at line %d@." line
+    Format.printf "No allocs for test '%s'@." test_name
   else
-    Format.printf "Some allocs at line %d@." line
+    Format.printf "Some allocs for test '%s'@." test_name
 
-let[@inline never] check_not_many_allocs max line f x y expected_res =
+let[@inline never] check_not_many_allocs max test_name f x y expected_res =
   let before = minor_words () in
   let f_res = (f[@inlined never]) x y in
   let after = minor_words () in
   let diff = after -. before in
   let b = f_res = expected_res in
   if not b then
-    Format.printf "Bad assert at line %d@." line
+    Format.printf "Bad assert for test '%s'@." test_name
   else if diff <= max then
-    Format.printf "Less than %f allocs at line %d@." max line
+    Format.printf "Less than %f allocs for test '%s'@." max test_name
   else
-    Format.printf "Too many allocs at line %d@." line
+    Format.printf "Too many allocs for test '%s'@." test_name
 
 module Complex = struct
   type t = { re: float; im: float }
@@ -88,8 +88,8 @@ let () =
     done;
     !r = res
   in
-  check_no_alloc __LINE__ aux 0 1l 2l;
-  check_no_alloc __LINE__ aux 5 1l 7l;
+  check_no_alloc "int32-loop-1" aux 0 1l 2l;
+  check_no_alloc "int32-loop-2" aux 5 1l 7l;
   (* Check int64s (working) *)
   let aux n y res =
     let r = ref y in
@@ -98,8 +98,8 @@ let () =
     done;
     !r = res
   in
-  check_no_alloc __LINE__ aux 0 1L 2L;
-  check_no_alloc __LINE__ aux 5 1L 7L;
+  check_no_alloc "int64-loop-1" aux 0 1L 2L;
+  check_no_alloc "int64-loop-2" aux 5 1L 7L;
   (* Check nativeints (working) *)
   let aux n y res =
     let r = ref y in
@@ -108,8 +108,8 @@ let () =
     done;
     !r = res
   in
-  check_no_alloc __LINE__ aux 0 1n 2n;
-  check_no_alloc __LINE__ aux 5 1n 7n;
+  check_no_alloc "nativeint-loop-1" aux 0 1n 2n;
+  check_no_alloc "nativeint-loop-2" aux 5 1n 7n;
   (* Check floats *)
   let aux n y res =
     let r = ref y in
@@ -118,8 +118,8 @@ let () =
     done;
     !r *. 2. = res
   in
-  check_no_alloc __LINE__ aux 0 1. 4.;
-  check_no_alloc __LINE__ aux 5 1. 14.;
+  check_no_alloc "float-loop-1" aux 0 1. 4.;
+  check_no_alloc "float-loop-2" aux 5 1. 14.;
   (* Check floats again,
      As of writing this test, floats in a loop are unboxed iff the first
      outisde *after* the loop can also make use of the unboxed version. This
@@ -136,8 +136,8 @@ let () =
     done;
     !r
   in
-  check_not_many_allocs 5. __LINE__ aux 1_000 0. 1_000.;
-  check_not_many_allocs 5. __LINE__ aux 10_000 0. 10_000.;
+  check_not_many_allocs 5. "float-hard-1" aux 1_000 0. 1_000.;
+  check_not_many_allocs 5. "float-hard-2" aux 10_000 0. 10_000.;
   (* Check tuple.
      Not currently unboxed. *)
   let aux n ((_x, _y) as t) res =
@@ -149,8 +149,8 @@ let () =
     let x, y = !r in
     x - y = res
   in
-  check_no_alloc __LINE__ aux 0 (1,2) 1;
-  check_no_alloc __LINE__ aux 1 (1,2) ~-1;
+  check_no_alloc "tuple-loop-1" aux 0 (1,2) 1;
+  check_no_alloc "tuple-loop-2" aux 1 (1,2) ~-1;
   (* Check nested tuples.
      Not currently unboxed. *)
   let aux n (((_x, _y), _z) as t) res =
@@ -162,8 +162,8 @@ let () =
     let (x, y), z = !r in
     x + (y - z) = res
   in
-  check_no_alloc __LINE__ aux 0 ((1,2),3) 4;
-  check_no_alloc __LINE__ aux 1 ((1,2),3) 2;
+  check_no_alloc "tuple-loop-3" aux 0 ((1,2),3) 4;
+  check_no_alloc "tuple-loop-4" aux 1 ((1,2),3) 2;
   (* Check tuple+floats.
      Not unboxed yet. *)
   let aux n ((_x, _y) as t) res =
@@ -175,8 +175,8 @@ let () =
     let x, y = !r in
     x +. y = res
   in
-  check_no_alloc __LINE__ aux 0 (1.,2.) 2.;
-  check_no_alloc __LINE__ aux 1 (1.,2.) 6.;
+  check_no_alloc "tuple-float-1" aux 0 (1.,2.) 2.;
+  check_no_alloc "tuple-float-2" aux 1 (1.,2.) 6.;
   (* Check blocks and floats.
      Not unboxed yet. *)
   let aux n (c1, c2) res =
@@ -191,8 +191,8 @@ let () =
     in
     f = res
   in
-  check_no_alloc __LINE__ aux 0 (Complex.zero, Complex.one) 0.;
-  check_no_alloc __LINE__ aux 1 (Complex.zero, Complex.one) 2.;
+  check_no_alloc "complex-loop-1" aux 0 (Complex.zero, Complex.one) 0.;
+  check_no_alloc "complex-loop-2" aux 1 (Complex.zero, Complex.one) 2.;
   (* Check variants.
      Not unboxed yet. *)
   let aux n x res =
@@ -209,8 +209,8 @@ let () =
     in
     r = res
   in
-  check_no_alloc __LINE__ aux 0 0 0;
-  check_no_alloc __LINE__ aux 1 0 13;
+  check_no_alloc "variant-loop-1" aux 0 0 0;
+  check_no_alloc "variant-loop-2" aux 1 0 13;
   (* Check more complicated variant.
      Not unboxed yet. *)
   let aux n (a, b) res =
@@ -231,10 +231,10 @@ let () =
     in
     e = res
   in
-  check_no_alloc __LINE__ aux 0 (1,2) 50;
-  check_no_alloc __LINE__ aux 1 (1,2) 1;
-  check_no_alloc __LINE__ aux 2 (1,2) 3;
-  check_no_alloc __LINE__ aux 3 (1,2) 42;
+  check_no_alloc "variant-loop-3" aux 0 (1,2) 50;
+  check_no_alloc "variant-loop-4" aux 1 (1,2) 1;
+  check_no_alloc "variant-loop-5" aux 2 (1,2) 3;
+  check_no_alloc "variant-loop-6" aux 3 (1,2) 42;
   (* END *)
   ()
 
